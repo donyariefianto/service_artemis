613d2aa9c2a62feaa28159aedd42c92fcc3233bd
donyariefianto <donyariefianto98@gmail.com>
2024-10-02 14:51:30 +0700

    .

--- app/middleware/proxy_middleware.ts
+++ app/middleware/proxy_middleware.ts
@@ -8,25 +8,25 @@ export default class ProxyMiddleware {
     /**
      * Middleware logic goes here (before the next call)
      */
-    if (ctx.request.url().startsWith('/drive/')) {
-      const minio_service = env.get('MINIO_SERVICE')
-      const proxy = createProxyMiddleware({
-        target: minio_service,
-        changeOrigin: true,
-        pathRewrite: {
-          '^/drive': '', // Remove /proxy prefix when forwarding
-        },
+    
+    const minio_service = env.get('MINIO_SERVICE')
+    const proxy = createProxyMiddleware({
+      target: minio_service,
+      changeOrigin: true,
+      pathRewrite: {
+        '^/drive': '', // Remove /proxy prefix when forwarding
+      },
+    })
+    return new Promise((resolve, reject) => {
+      proxy(ctx.request.request, ctx.response.response, (err: any) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve()
+        }
       })
-      return new Promise((resolve, reject) => {
-        proxy(ctx.request.request, ctx.response.response, (err: any) => {
-          if (err) {
-            reject(err)
-          } else {
-            resolve()
-          }
-        })
-      })
-    }
+    })
+    
     /**
      * Call next method in the pipeline and return its output
      */
